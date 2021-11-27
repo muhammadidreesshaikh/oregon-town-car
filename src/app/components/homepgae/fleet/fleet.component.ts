41e38e4cc60dd379c96247c73a3b4a92772f2d47
muhammadidreesshaikh <idreesiqbal104@gmail.com>
2021-11-27 12:43:18 +0500

    fleet component done

--- src/app/components/homepgae/fleet/fleet.component.ts
+++ src/app/components/homepgae/fleet/fleet.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FleetComponent implements OnInit {
 
+  cards: any = [
+    {
+      id: 1,
+      name: 'Rentals',
+      image: '../../../../assets/img/fleet1.png',
+      dynamic: 'Efficient',
+      content: 'Independence',
+    },
+    {
+      id: 2,
+      name: 'Bikes & Scooters',
+      image: '../../../../assets/img/fleet2.png',
+      dynamic: 'Efficient',
+      content: 'Eco-friendly',
+    },
+    {
+      id: 3,
+      name: 'Shared Rides',
+      image: '../../../../assets/img/fleet3.png',
+      dynamic: 'Budget-friendly',
+      content: 'Private',
+    },
+    {
+      id: 4,
+      name: 'Wait & Save',
+      image: '../../../../assets/img/fleet4.png',
+      dynamic: 'Budget-friendly',
+      content: 'Eco-friendly',
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
