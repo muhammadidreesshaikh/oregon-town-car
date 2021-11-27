c07ac627e152d139e4fde63d896af8ec75202004
muhammadidreesshaikh <idreesiqbal104@gmail.com>
2021-11-27 20:04:53 +0500

    business-move  component done

--- src/app/components/homepgae/business-move/business-move.component.ts
+++ src/app/components/homepgae/business-move/business-move.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusinessMoveComponent implements OnInit {
 
+  data: any = [
+    {
+      id: 1,
+      image: '../../../../assets/img/business1.jpg',
+      name: 'Courtesy rides',
+    },
+    {
+      id: 1,
+      image: '../../../../assets/img/business2.jpg',
+      name: 'Group transportation',
+    },
+    {
+      id: 1,
+      image: '../../../../assets/img/business3.jpg',
+      name: 'Employee rides',
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
