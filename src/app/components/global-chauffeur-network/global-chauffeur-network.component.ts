24237f012f4fd6eb884559e477c9408d975ededc
muhammadidreesshaikh <idreesiqbal104@gmail.com>
2021-12-07 21:48:34 +0500

    fixes before deployment

--- src/app/components/global-chauffeur-network/global-chauffeur-network.component.ts
+++ src/app/components/global-chauffeur-network/global-chauffeur-network.component.ts
@@ -10,19 +10,19 @@ export class GlobalChauffeurNetworkComponent implements OnInit {
   data: any = [
     {
       id: 1,
-      image: '../../../assets/img/global1.jpg',
+      image: 'assets/img/global1.jpg',
       name: 'Run it all from one place',
       text: 'It’s a digital “headquarters” for all your company’s ground transportation.',
     },
     {
       id: 2,
-      image: '../../../assets/img/global2.jpg',
+      image: 'assets/img/global2.jpg',
       name: 'Streamline your expensing',
       text: 'Say Hello To Automated Expense Reporting & Goodbye To Manual Processes. Get a clear view into all your trip activity and automate billing, expensing, and reporting.',
     },
     {
       id: 3,
-      image: '../../../assets/img/global3.jpg',
+      image: 'assets/img/global3.jpg',
       name: 'Support',
       text: 'Customer service team 24/7',
     }
--- src/app/components/homepgae/business-move/business-move.component.ts
+++ src/app/components/homepgae/business-move/business-move.component.ts
@@ -10,17 +10,17 @@ export class BusinessMoveComponent implements OnInit {
   data: any = [
     {
       id: 1,
-      image: '../../../../assets/img/business1.jpg',
+      image: '../assets/img/business1.jpg',
       name: 'Courtesy rides',
     },
     {
       id: 1,
-      image: '../../../../assets/img/business2.jpg',
+      image: '../assets/img/business2.jpg',
       name: 'Group transportation',
     },
     {
       id: 1,
-      image: '../../../../assets/img/business3.jpg',
+      image: '../assets/img/business3.jpg',
       name: 'Employee rides',
     }
   ]
