625bed249e61ca2511629f55f603a69db3a9d19b
muhammadidreesshaikh <idreesiqbal104@gmail.com>
2021-12-07 22:36:12 +0500

    fix on img issue loading

--- src/app/components/homepgae/fleet/fleet.component.ts
+++ src/app/components/homepgae/fleet/fleet.component.ts
@@ -11,28 +11,28 @@ export class FleetComponent implements OnInit {
     {
       id: 1,
       name: 'Rentals',
-      image: '../../assets/img/fleet1.png',
+      image: 'assets/img/fleet1.png',
       dynamic: 'Efficient',
       content: 'Independence',
     },
     {
       id: 2,
       name: 'Bikes & Scooters',
-      image: '../../assets/img/fleet2.png',
+      image: 'assets/img/fleet2.png',
       dynamic: 'Efficient',
       content: 'Eco-friendly',
     },
     {
       id: 3,
       name: 'Shared Rides',
-      image: '../../assets/img/fleet3.png',
+      image: 'assets/img/fleet3.png',
       dynamic: 'Budget-friendly',
       content: 'Private',
     },
     {
       id: 4,
       name: 'Wait & Save',
-      image: '../../assets/img/fleet4.png',
+      image: 'assets/img/fleet4.png',
       dynamic: 'Budget-friendly',
       content: 'Eco-friendly',
     }
